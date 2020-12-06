ce71f492fb2d714d97bbf98ac241cc5928eb00e0
joshspicer <hello@joshspicer.com>
2020-12-06 16:34:14 +0000

    add codespaces devcontainer

---
+++ .devcontainer/jekyll-dep.sh
@@ -0,0 +1,7 @@
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+apt-get install ruby-full build-essential zlib1g-dev
+gem install jekyll bundler
+
