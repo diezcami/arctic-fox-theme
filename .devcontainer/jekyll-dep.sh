975aa16a7c110ad7739d46d7cf6412d41b7dd8d8
joshspicer <hello@joshspicer.com>
2020-12-06 23:56:27 +0000

    add back dashes that i guess are important

--- .devcontainer/jekyll-dep.sh
+++ .devcontainer/jekyll-dep.sh
@@ -3,12 +3,16 @@ echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
+# Ruby Prereqs
 apt-get install -y software-properties-common  build-essential zlib1g-dev
 apt-add-repository -y ppa:brightbox/ruby-ng
 apt update
-apt install -y ruby2.7 ruby-switch
+apt install -y ruby2.7-dev ruby-switch
 ruby-switch --set ruby2.7
 
+# Install Jekyll
+gem install jekyll bundler jekyll-redirect-from
+# Nice to have
+apt install -y vim
 
-gem install jekyll bundler
-
+echo "done" >> /root/workspace/jekyll-dep.log
