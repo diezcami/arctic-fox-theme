c1f96ddf12f203bfe9ab2f996830e7beb837cd15
joshspicer <hello@joshspicer.com>
2020-12-06 16:46:34 +0000

    git commit -m

--- .devcontainer/jekyll-dep.sh
+++ .devcontainer/jekyll-dep.sh
@@ -2,6 +2,13 @@ echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
-apt-get install ruby-full build-essential zlib1g-dev
+
+apt-get install -y software-properties-common  build-essential zlib1g-dev
+apt-add-repository -y ppa:brightbox/ruby-ng
+apt update
+apt install -y ruby2.7 ruby-switch
+ruby-switch --set ruby2.7
+
+
 gem install jekyll bundler
 
